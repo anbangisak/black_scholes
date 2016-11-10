b3f326181e55381dcf1586cd3b52d2fee04503d3
Guna Anban <guna@egrovesystems.com>
2016-11-11 01:10:55 +0530

    all input page and show page added

--- app/controllers/scholes_controller.rb
+++ app/controllers/scholes_controller.rb
@@ -8,7 +8,7 @@ class ScholesController < ApplicationController
   def scholes_update_price
     begin
       cal_or_put_option = @schole.cal_option_val ? "c" : "p" 
-      @display_price = black_scholes(cal_or_put_option,@schole.stock_price.to_f, params[:strike_price].to_f, @schole.years.to_f, params[:risk_free].to_f, params[:volatility].to_f)
+      @display_price = Schole.black_scholes(cal_or_put_option,@schole.stock_price.to_f, params[:strike_price].to_f, @schole.years.to_f, params[:risk_free].to_f, params[:volatility].to_f)
       @message = "successful"
       @status = 200
     rescue => e
@@ -42,26 +42,25 @@ class ScholesController < ApplicationController
     end
   end
 
+  def scholes_all
+  end
 
-  private
-    def cnd(x)
-      a1, a2, a3, a4, a5 = 0.31938153, -0.356563782, 1.781477937, -1.821255978, 1.330274429
-      l = x.abs
-      k = 1.0 / (1.0 + 0.2316419 * l)
-      w = 1.0 - 1.0 / Math.sqrt(2*Math::PI)*Math.exp(-l*l/2.0) * (a1*k + a2*k*k + a3*(k**3) + a4*(k**4) + a5*(k**5))
-      w = 1.0 - w if x < 0
-      return w
-    end
-
-    def black_scholes(callPutFlag, s, x, t, r, v)
-      d1 = (Math.log(s/x)+(r+v*v/2.0)*t)/(v*Math.sqrt(t))
-      d2 = d1-v*Math.sqrt(t)
-      if callPutFlag == 'c'
-        s*cnd(d1)-x*Math.exp(-r*t)*cnd(d2)
+  def scholes_save_all
+    @schole = current_user.scholes.new(scholes_save_all_params)
+    respond_to do |format|
+      if @schole.save
+        format.html { redirect_to scholes_path, notice: 'Scholes was successfully created.' }
       else
-        x*Math.exp(-r*t)*cnd(-d2)-s*cnd(-d1)
+        format.html { render :scholes_all }
       end
     end
+  end
+
+  def index
+    @scholes = Schole.all
+  end
+
+  private
 
     def scholes_params
       params.permit(:stock_price, :call_put_scholes, :years)
@@ -69,6 +68,11 @@ class ScholesController < ApplicationController
 
     def schole_save_params
       {stock_price: params[:stock_price], cal_option_val: params[:call_put_scholes] == "call", put_option_val: params[:call_put_scholes] == "put", years: params[:years]}
+    end
+
+    def scholes_save_all_params
+      params.permit(:stock_price, :call_put_scholes, :years, :strike_price, :risk_free, :volatility )
+      {stock_price: params[:stock_price], cal_option_val: params[:call_put_scholes] == "call", put_option_val: params[:call_put_scholes] == "put", years: params[:years], strike_price: params[:strike_price], risk_free: params[:risk_free], volatility: params[:volatility]}
     end
 
     def scholes_vals
